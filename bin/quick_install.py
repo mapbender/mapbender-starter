935a9dda8ffcbef049f62f7df6778be5c502dcfa
Astrid Emde <astrid.emde@wheregroup.com>
2014-07-29 16:42:41 +0200

    Update quick_install.py
    changed parameter names

--- bin/quick_install.py
+++ bin/quick_install.py
@@ -24,7 +24,7 @@ https://raw.githubusercontent.com/mapbender/mapbender-starter/develop/bin/quick_
 
 - Install develop branch, but use admin <admin@example.com> with password admin
 
-    curl -sSL http://bit.ly/1tQvo5i | python - --username=admin --useremail=admin@example.com --userpassword=admin
+    curl -sSL http://bit.ly/1tQvo5i | python - --username=admin --email=admin@example.com --password=admin
 """
 
 import argparse
