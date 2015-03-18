95d8a3eee1693c220b911925da83e73a639a68b1
Stefan Winkelmann <stefan.winkelmann@wheregroup.com>
2015-03-18 09:52:53 +0100

    created digitizer branch

--- application/app/AppKernel.php
+++ application/app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             new Mapbender\WmsBundle\MapbenderWmsBundle(),
             new Mapbender\ManagerBundle\MapbenderManagerBundle(),
             new Mapbender\PrintBundle\MapbenderPrintBundle(),
+            new Mapbender\DigitizerBundle\MapbenderDigitizerBundle(),
 
             // OWSProxy3 bundles
             new OwsProxy3\CoreBundle\OwsProxy3CoreBundle(),
