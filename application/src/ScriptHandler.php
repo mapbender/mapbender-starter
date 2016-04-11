ae1f3bfc5221e8474395d54a23291660ab2fd91e
Andriy Oblivantsev <eslider@gmail.com>
2016-04-11 19:47:31 +0200

    Improve installation script handler

--- application/src/ScriptHandler.php
+++ application/src/ScriptHandler.php
@@ -1,7 +1,4 @@
 <?php
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\PhpExecutableFinder;
 use Composer\Script\CommandEvent;
 
 /**
@@ -19,45 +16,168 @@ class ScriptHandler
      */
     public static function checkConfiguration(CommandEvent $event)
     {
-        $rootPath              = realpath(__DIR__ . "/..");
+        $rootPath              = self::getSymfonyRootPath();
         $configPath            = $rootPath . "/app/config/";
         $configurationBaseFile = $configPath . "parameters.yml.dist";
         $configurationFile     = $configPath . "parameters.yml";
         $isNewInstall          = !file_exists($configurationFile);
-        $isWindows             = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
         //$configuration         = file_get_contents($configurationBaseFile);
 
         if ($isNewInstall) {
-            $hasApplicationSubFolder = is_dir($rootPath . "/../.git");
 
-            if ($hasApplicationSubFolder) {
-                echo `cd ..`;
-            }
-
-            echo `git submodule sync`;
-            echo `git submodule update --init --recursive`;
-
-            if ($hasApplicationSubFolder) {
-                echo `cd "$rootPath"`;
-            }
+            self::updateSubmodules();
 
             copy($configurationBaseFile, $configurationFile);
 
-            echo `php app/console doctrine:database:create`;
-            echo `php app/console doctrine:schema:create`;
-            echo `php app/console app/console doctrine:schema:update --force`;
-            echo `php app/console fom:user:resetroot --username root --password root --email root@localhost --silent`;
-            echo `php app/console doctrine:fixtures:load --fixtures=./mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/ --append`;
-            echo `php app/console doctrine:fixtures:load --fixtures=./mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Application/ --append`;
+            self::createDatabase();
+            self::resetRootLogin();
+            self::importExampleApplications();
+            self::updateEpsgCodes();
         }
 
+        self::clearCache();
+    }
 
+    /**
+     * Is windows
+     *
+     * @return bool
+     */
+    protected static function isWindows()
+    {
+        static $isWindows;
+        if ($isWindows === null) {
+            $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
+        }
+        return $isWindows;
+    }
 
+    /**
+     * Clear cache but let sessions alive
+     */
+    protected static function clearCache()
+    {
+        $isWindows = self::isWindows();
+        $cachePath = "app/cache";
         if (!$isWindows) {
-            echo `rm -rf app/cache/*`;
-            echo `chmod -R ug+w app/cache`;
-            echo `chmod -R ug+w app/logs`;
-            echo `chmod -R ug+w web/uploads`;
+            self::printStatus("Clear cache");
+            foreach (glob($cachePath . "/*/*") as $filePath) {
+                $fileInfo = explode("/", $filePath);
+                $fileName = end($fileInfo);
+                if ($fileName == "sessions") {
+                    continue;
+                }
+                //$filePath = escapeshellarg($filePath);
+                echo `rm -rf $filePath`;
+            }
         }
+    }
+
+    /**
+     * Enable write cache, logs and upload folder by user and group
+     */
+    protected static function allowWriteLogs()
+    {
+        if (!self::isWindows()) {
+            self::printStatus("Enable write cache, logs and upload for user and user group");
+
+            echo `chmod -R ug+wX app/cache`;
+            echo `chmod -R ug+wX app/logs`;
+            echo `chmod -R ug+wX web/uploads`;
+        }
+    }
+
+    /**
+     * Crate database, schema and force update
+     */
+    protected static function createDatabase()
+    {
+        self::printStatus("Create and prepare database");
+
+        echo `php app/console doctrine:database:create`;
+        echo `php app/console doctrine:schema:create`;
+        //echo `php app/console doctrine:schema:update --force`;
+    }
+
+    /**
+     * Reset root user login
+     *
+     * @param string $userName
+     * @param string $password
+     * @param string $userEmail
+     */
+    protected static function resetRootLogin($userName = "root", $password = "root", $userEmail = "root@localhost")
+    {
+        $userName  = escapeshellarg($userName);
+        $userEmail = escapeshellarg($userEmail);
+        $password  = escapeshellarg($password);
+
+        self::printStatus("Reset user password");
+
+        `php app/console fom:user:resetroot --username $userName --password $password --email $userEmail --silent`;
+
+        self::printStatus("ATTENTION");
+        echo "User $userName account password is: $password. Don't forget to change it!\n";
+    }
+
+    /**
+     * Update submodules
+     */
+    protected static function updateSubmodules()
+    {
+        $rootPath                = self::getSymfonyRootPath();
+        $hasApplicationSubFolder = is_dir($rootPath . "/../.git");
+        if ($hasApplicationSubFolder) {
+            echo `cd ..`;
+        }
+
+        self::printStatus("Update submodules");
+
+        echo `git submodule sync`;
+        echo `git submodule update --init --recursive`;
+
+        if ($hasApplicationSubFolder) {
+            echo `cd "$rootPath"`;
+        }
+    }
+
+    /**
+     * Get symfony root path
+     *
+     * @return string
+     */
+    protected static function getSymfonyRootPath()
+    {
+        static $path;
+        if (!$path) {
+            $path = realpath(__DIR__ . "/..");
+        }
+        return $path;
+    }
+
+    /**
+     * Import or update EPSG codes
+     */
+    protected static function updateEpsgCodes()
+    {
+        self::printStatus("Update EPSG codes");
+        echo `php app/console doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/ --append`;
+    }
+
+    /**
+     * Import example applications from "app/config/mapbender.yml" file
+     */
+    protected static function importExampleApplications()
+    {
+        self::printStatus("Import example mapbender applications");
+        echo `php app/console doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Application/ --append`;
+    }
+
+    /**
+     * @return string
+     */
+    protected static function printStatus($title)
+    {
+        echo "\n[$title]\n";
     }
 }
