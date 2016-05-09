162136cad0e2a793cd23d55ef10b70a132f2bc25
Andriy Oblivantsev <eslider@gmail.com>
2016-05-09 10:53:32 +0200

    Publish ComposerBootstrap methods for using by composer event management configuration

--- application/src/ComposerBootstrap.php
+++ application/src/ComposerBootstrap.php
@@ -48,7 +48,7 @@ class ComposerBootstrap
      *
      * @return bool
      */
-    protected static function isWindows()
+    public static function isWindows()
     {
         static $isWindows;
         if ($isWindows === null) {
@@ -81,7 +81,7 @@ class ComposerBootstrap
     /**
      * Enable write cache, logs and upload folder by user and group
      */
-    protected static function allowWriteLogs()
+    public static function allowWriteLogs()
     {
         if (!self::isWindows()) {
             self::printStatus("Enable write cache, logs and upload for user and user group");
@@ -95,7 +95,7 @@ class ComposerBootstrap
     /**
      * Crate database, schema and force update
      */
-    protected static function createDatabase()
+    public static function createDatabase()
     {
         self::printStatus("Create and prepare database");
 
@@ -128,7 +128,7 @@ class ComposerBootstrap
     /**
      * Update submodules
      */
-    protected static function updateSubmodules()
+    public static function updateSubmodules()
     {
         $rootPath                = self::getSymfonyRootPath();
         $hasApplicationSubFolder = is_dir($rootPath . "/../.git");
@@ -163,7 +163,7 @@ class ComposerBootstrap
     /**
      * Import or update EPSG codes
      */
-    protected static function updateEpsgCodes()
+    public static function updateEpsgCodes()
     {
         self::printStatus("Update EPSG codes");
         echo `php app/console doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/ --append`;
@@ -172,7 +172,7 @@ class ComposerBootstrap
     /**
      * Import example applications from "app/config/mapbender.yml" file
      */
-    protected static function importExampleApplications()
+    public static function importExampleApplications()
     {
         self::printStatus("Import example mapbender applications");
         echo `php app/console doctrine:fixtures:load --fixtures=mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Application/ --append`;
