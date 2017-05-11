4386f7ff972a0aa7d39e7cdf65e397572d74a8d6
Andrey Oblivantsev <eslider@gmail.com>
2017-05-11 16:10:45 +0200

    Let config.php by distributing or build a package
    Closes: https://github.com/mapbender/mapbender/issues/526

--- application/src/ComposerBootstrap.php
+++ application/src/ComposerBootstrap.php
@@ -429,7 +429,6 @@ class ComposerBootstrap
 
                      "web/app_test.php",
                      "web/index.php",
-                     "web/config.php",
 
                      "app/cache/*",
                      "app/logs/*",
