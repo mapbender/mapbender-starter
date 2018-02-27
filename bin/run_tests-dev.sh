787fb31a0c4aa57dd20f52d9989fbfa873de1630
wg-rolf <rolf.neuberger@wheregroup.com>
2018-02-27 23:58:06 +0100

    Reformat

--- bin/run_tests-dev.sh
+++ bin/run_tests-dev.sh
@@ -4,4 +4,8 @@ SCRIPTPATH="${SCRIPTPATH:-${BASH_SOURCE[0]}}"
 SCRIPTDIR="$(dirname ${SCRIPTPATH})"
 # use composer-installed phpunit
 # regular phpunit won't really work
-"${SCRIPTDIR}/../application/bin/phpunit" -c "${SCRIPTDIR}/../application/app/" "--bootstrap=${SCRIPTDIR}/../application/app/autoload.php" --exclude-group=functional "$@"
+"${SCRIPTDIR}/../application/bin/phpunit" \
+  -c "${SCRIPTDIR}/../application/app/" \
+  "--bootstrap=${SCRIPTDIR}/../application/app/autoload.php" \
+  --exclude-group=functional \
+  "$@"
