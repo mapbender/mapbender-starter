66165772281c550d5bbc2f867d5dcb6d0920f42e
wg-rolf <rolf.neuberger@wheregroup.com>
2018-06-14 17:35:37 +0200

    Remove automatic server:run at end of bootstrap, echo a message instead

--- bootstrap.bat
+++ bootstrap.bat
@@ -2,4 +2,6 @@ cd application
 php bin/composer install -o
 php bin/composer init-example
 php app/console assets:install
-php app/console server:run
+echo Bootstrap finished!
+echo If you want to run the builtin development server, run:
+echo php app/console server:run
