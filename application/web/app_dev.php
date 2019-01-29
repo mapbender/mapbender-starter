5d9c693cc7379b16347818931010e246b978847f
rpour <a.rashid.pour@wheregroup.com>
2019-01-29 15:11:40 +0100

    Removed old files and replaced some

--- application/web/app_dev.php
+++ application/web/app_dev.php
@@ -1,35 +1,28 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
-require_once(dirname(__FILE__) . '/../src/ComposerBootstrap.php');
-$usePrebuilt = ComposerBootstrap::isWindows();
+use Symfony\Component\HttpFoundation\Request;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
+// for more information
 //umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-   || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-   || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
 ) {
-   header('HTTP/1.0 403 Forbidden');
-   exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-if ($usePrebuilt) {
-    $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-} else {
-    $loader = require_once __DIR__.'/../app/autoload.php';
-}
+require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 
-require_once __DIR__.'/../app/AppKernel.php';
-
 $kernel = new AppKernel('dev', true);
-if ($usePrebuilt) {
+if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
 $request = Request::createFromGlobals();
