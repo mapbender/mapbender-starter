7b013168533cda3e5781a57ba772f131c3b4929a
werrolf <rolf.neuberger@wheregroup.com>
2019-11-08 12:23:51 +0100

    Update sensio/distribution-bundle from 4.0.39 => 5.0.22

--- application/web/app.php
+++ application/web/app.php
@@ -2,8 +2,8 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
-
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once(__DIR__ . '/../app/autoload.php');
+require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
--- application/web/app_dev.php
+++ application/web/app_dev.php
@@ -19,10 +19,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
+$loader = require_once(__DIR__ . '/../app/autoload.php');
 if ($usePrebuilt) {
-    $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-} else {
-    $loader = require_once __DIR__.'/../app/autoload.php';
+    require_once __DIR__.'/../app/bootstrap.php.cache';
 }
 Debug::enable();
 
