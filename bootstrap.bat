f94dffb400b8a8606ca2ed978b6b7ae12d0b8ae1
werrolf <rolf.neuberger@wheregroup.com>
2021-08-27 14:13:39 +0200

    Fix component mirroring not triggering in bootstrap script

--- bootstrap.bat
+++ bootstrap.bat
@@ -3,7 +3,7 @@ php bin/composer install -o --no-scripts --no-suggest
 php bin/composer init-example
 php app/console assets:install
 php app/console mapbender:database:init -v
-php bin/composer run post-autoload-dump
+php bin/composer run post-install-cmd
 echo Bootstrap finished!
 echo If you want to run the builtin development server, run:
 echo cd application
