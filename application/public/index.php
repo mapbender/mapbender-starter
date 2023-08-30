fa6f80009c8544be6d104972305d5c1f2820dc47
Thorsten Hack <thorsten.hack@wheregroup.com>
2023-08-30 13:35:54 +0200

    Update symfony/framework-bundle recipe

--- application/public/index.php
+++ application/public/index.php
@@ -1,23 +1,6 @@
 <?php
 
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
-
-require dirname(__DIR__).'/config/bootstrap.php';
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 $kernelClass = $_ENV['KERNEL_CLASS'];
 if (!$kernelClass) {
@@ -27,8 +10,6 @@ if (!class_exists($kernelClass)) {
     throw new Exception("KERNEL_CLASS environment variable does not refer to an existing class. Check the spelling and make sure composer autoload is setup correctly.");
 }
 
-$kernel = new $kernelClass($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) use ($kernelClass) {
+    return new $kernelClass($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+};
