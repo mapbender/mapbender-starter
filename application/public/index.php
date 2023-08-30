6342cd2a98a83fa8cea6efdf26404cb71553e15d
Thorsten Hack <thorsten.hack@wheregroup.com>
2023-08-30 14:04:07 +0200

    Setup kernel and console to symfony default

--- application/public/index.php
+++ application/public/index.php
@@ -1,15 +1,9 @@
 <?php
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+use App\Kernel;
 
-$kernelClass = $_ENV['KERNEL_CLASS'];
-if (!$kernelClass) {
-    throw new Exception("KERNEL_CLASS environment variable not found. Please add the FQCN of your Kernel class to your .env file");
-}
-if (!class_exists($kernelClass)) {
-    throw new Exception("KERNEL_CLASS environment variable does not refer to an existing class. Check the spelling and make sure composer autoload is setup correctly.");
-}
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) use ($kernelClass) {
-    return new $kernelClass($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
--- application/src/Kernel.php
+++ application/src/Kernel.php
@@ -2,10 +2,8 @@
 
 namespace App;
 
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Mapbender\BaseKernel;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
 }
